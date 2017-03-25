da05925d38f973db4c898bd497d8b80f2dbe26ac
Сергей Савенко <taisp2011@yandex.ru>
2017-03-26 01:32:24 +0200

    Fixed opened/closed resizable area

--- skin/js/page.js
+++ skin/js/page.js
@@ -2,14 +2,20 @@
  * Created by sergejsavenko on 2/25/17.
  */
 function simple_screenshot_overlay_toggle() {
-	jQuery('body').append('<div class="simple_screenshot_bg"></div>');
-	jQuery('.simple_screenshot_bg').Jcrop({
-		onSelect: function (c) {
-			chrome.extension.sendMessage({
-				screenshotCoords: c,
-			});
-			jQuery('.simple_screenshot_bg').remove();
-		},
-	});
+
+	if (jQuery('body').find('.simple_screenshot_bg').length == 0) {
+		jQuery('body').append('<div class="simple_screenshot_bg"></div>');
+		jQuery('.simple_screenshot_bg').Jcrop({
+			onSelect: function (c) {
+				chrome.extension.sendMessage({
+					screenshotCoords: c,
+				});
+				jQuery('.simple_screenshot_bg').remove();
+			},
+		});
+	} else {
+		jQuery('.simple_screenshot_bg').remove();
+
+	}
 }
 
